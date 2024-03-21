1e41cc2284180ea6a90210eb02eb2780017711e3
bayleaflet <bayleejchristensen@gmail.com>
2024-03-21 16:03:16 -0700

    First commit

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# BJC, Original Author
+# 3/21/2024, V 1.1.1
+# Purpose is to allow you to enter differerent calculators to help you
+# understand equations
+
+def main():
+    print("Thank you for entering the calculator quiz. Enter X at any time to exit")
+    print("You have several options for this calculator. There are multiple equations that can be used.")
+    print("Hardy Weinberg Equilibrium: H")
+    print("Breeder's Calculation: B")
+    print("Calculating allele and genotypic frequency: F")
+
+    selection = input("Please make your selection: ")
+
